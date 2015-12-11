e73cc799f1d38587f4c345785453d897612ca3f0
Sunny-guangge <18611021761@163.com>
2015-12-11 10:46:03 +0800

    添加消息的公共类，删除之前建的类

---
+++ xmppChat/IM/消息/view/MessageCell/CKMessageCommonTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CKMessageCommonTableViewCell.h
+//  xmppChat
+//
+//  Created by user on 15/12/11.
+//  Copyright © 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKMessageCommonTableViewCell : UITableViewCell
+
+@end
---
+++ xmppChat/IM/消息/view/MessageCell/CKMessageCommonTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CKMessageCommonTableViewCell.m
+//  xmppChat
+//
+//  Created by user on 15/12/11.
+//  Copyright © 2015年 user. All rights reserved.
+//
+
+#import "CKMessageCommonTableViewCell.h"
+
+@implementation CKMessageCommonTableViewCell
+
+@end
