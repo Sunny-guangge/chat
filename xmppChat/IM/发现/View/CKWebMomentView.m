fa8d9e2cf4a257610dd7369ac991afe4b8ff2c9f
Sunny-guangge <18611021761@163.com>
2015-12-23 10:46:23 +0800

    CKWebMomentView

--- xmppChat/IM/发现/View/CKWebMomentView.m
+++ xmppChat/IM/发现/View/CKWebMomentView.m
@@ -41,18 +41,30 @@
 {
     [super setFrame:frame];
     
-    
+    _imageView.frame = CGRectMake(5, 5, 40, 40);
+    _titleLabel.frame = CGRectMake(50, 5, frame.size.width - 55, 40);
+    _clickButton.frame = CGRectMake(0, 0, frame.size.width, frame.size.height);
 }
 
 - (void)setWeb:(CKWeb *)web
 {
+    _imageView.image = [UIImage imageNamed:web.web_ImageURL];
     
+    _titleLabel.text = web.web_Title;
 }
 
+- (void)didCLickWeb
+{
+    if (_delegate && [_delegate respondsToSelector:@selector(clickWebMomentWithWeb:)]) {
+        [_delegate clickWebMomentWithWeb:self.web];
+    }
+}
+
+#pragma mark - Getter
 - (UIImageView *)imageView
 {
     if (_imageView == nil) {
-        
+        _imageView = [[UIImageView alloc] init];
     }
     return _imageView;
 }
@@ -60,7 +72,10 @@
 - (UILabel *)titleLabel
 {
     if (_titleLabel == nil) {
-        
+        _titleLabel = [[UILabel alloc] init];
+        _titleLabel.numberOfLines = 2;
+        _titleLabel.textColor = [UIColor blackColor];
+        _titleLabel.font = [UIFont systemFontOfSize:14];
     }
     return _titleLabel;
 }
@@ -68,7 +83,8 @@
 - (UIButton *)clickButton
 {
     if (_clickButton == nil) {
-        
+        _clickButton = [[UIButton alloc] init];
+        [_clickButton addTarget:self action:@selector(didCLickWeb) forControlEvents:UIControlEventTouchUpInside];
     }
     return _clickButton;
 }
